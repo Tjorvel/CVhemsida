54ac83fc9577b237c68d8798d73af7006bda16c7
Tjorvel <bente_8_94@hotmail.com>
2016-05-03 09:58:22 +0200

    Logo_links

---
+++ CV_site/CV_site/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV_site.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Cv()
+        {
+            return View();
+        }
+        public ActionResult Kurser()
+        {
+            return View();
+        }
+        public ActionResult Github()
+        {
+            return View();
+        }
+    }
+}
